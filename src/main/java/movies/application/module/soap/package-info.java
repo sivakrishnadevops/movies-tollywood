45e16f4efe655992316fc1eb890317c44ccbbbb2
sivakrishna <sivakrishna.malepati@gmail.com>
2021-05-02 11:47:17 -0400

    devloped code on moves in tollywood

---
+++ src/main/java/movies/application/module/soap/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * SOAP client/server contract-last interface definitions of module.
+ * Define here the SOAP client/server interfaces that are going to be developed with a contract-last approach
+ * (i.e.: you start with the code).
+ * This interfaces will be annotated with {@code @WebService}
+ * and their methods will be annotated with {@code @WebMethod}.
+ * However, consider using the recommended contract-first development approach,
+ * where this SOAP client/server interface definition are generated from the WSDL that defines the SOAP service.
+ */
+package movies.application.module.soap;
---
+++ src/main/resources/wsdl/readme.txt
@@ -0,0 +1,5 @@
+
+SOAP client/server contract-first interface definitions of module.
+
+Any WSDL file placed in this wsdl package of the resources folder will generate the proper Java artifacts
+given that the cxf-codegen-plugin is properly configured in the project.
